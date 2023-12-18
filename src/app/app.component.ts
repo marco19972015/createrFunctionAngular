e6db59ad82eb3fabf494345fcb642333cec994ac
Marco Torres <marcoantonio.torres@icloud.com>
2023-12-18 10:56:17 -0500

    Create a few examples to showcase the of and from creation functions. I then created a new component to showcase the fromEvent creation function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'creation-functions';
+
+  // Define a variable for the subscription (this is so we can ubsubscribe from the variable)
+  sub!: Subscription;
+  subArray!: Subscription;
+  subFrom!: Subscription;
+  subString!: Subscription;
+
+  ngOnInit(){
+    // emits a next notification for each number
+    this.sub = of(2, 4, 6, 8).subscribe(item => {console.log('Value from of: ', item);});
+    // emits a single next notification
+    this.subArray = of([2, 4, 6, 8]).subscribe(item => {console.log('Value from of: ', item);});
+    // Passing an array in the from creation function emits next notification on all numbers
+    this.subFrom = from([20, 15, 10, 5]).subscribe({
+      next: item => console.log('From item: ', item),
+      error: err => console.log('From error: ', err),
+      complete: () => console.log('From completed')
+    });
+    this.subString = of('String 1', 'String 2', 'String 3').subscribe({
+      next: item => console.log('From subString: ', item),
+      error: err => console.log('From err: ', err),
+      complete: () => console.log('From completed in subString: ')
+    });
+  }
+
+  ngOnDestroy(){
+    // We then unsubscribe (this is good practice)
+    this.sub.unsubscribe();
+    this.subArray.unsubscribe();
+    this.subFrom.unsubscribe();
+    this.subString.unsubscribe();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FromEventComponent } from './from-event/from-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FromEventComponent
   ],
   imports: [
     BrowserModule
